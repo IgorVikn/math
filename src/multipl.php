1251dc76863637d417f97c8700c991f48f25a807
IgorVikn <igorvikn@gmail.com>
2021-05-13 18:17:53 +0300

    new package

---
+++ src/multipl.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace igorvikn\math;
+
+class Multipl 
+{
+	public function multiply($a, $b)
+	{
+		return $a * $b;
+	}
+
+}
+
+
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8bdfdf23c5bb99c03180c50823dee93382949598',
+    'name' => 'igorvikn/math',
+  ),
+  'versions' => 
+  array (
+    'igorvikn/math' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8bdfdf23c5bb99c03180c50823dee93382949598',
+    ),
+  ),
+);
